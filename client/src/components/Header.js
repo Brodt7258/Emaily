2a703bd7ff7efdad08bbadc31fda7f56eca1d044
Tim Dill <brodt7258@gmail.com>
2018-04-07 15:34:47 -0400

    track credits in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Payments from './Payments';
-import * as actions from '../actions';
 
 class Header extends Component {
   renderContent() {
@@ -16,7 +15,10 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>,
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits || 0 }
+          </li>,
+          <li key="3"><a href="/api/logout">Logout</a></li>
         ];
     }
   }
@@ -44,4 +46,4 @@ const mapStateToProps = ({ auth }) => {
   return { auth };
 }
 
-export default connect(mapStateToProps, actions)(Header);
+export default connect(mapStateToProps)(Header);
